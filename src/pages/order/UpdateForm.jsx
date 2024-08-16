fa91575a8867a3bbedefe8225bb46b0789bd3b7d
yongqi8899 <1243994415@qq.com>
2024-08-16 17:17:57 +0200

    add update order

--- src/pages/order/UpdateForm.jsx
+++ src/pages/order/UpdateForm.jsx
@@ -12,50 +12,65 @@ export default function UpdateForm() {
   const busy = navigation.state === "submitting";
   const { id } = useParams();
   const oders = useOutletContext();
-  const oder = oders.find((oder) => oder.id === +id);
+  const order = oders.find((oder) => oder.id === +id);
   return (
     <Form method="POST">
       <div className="m-auto card-body w-96">
-        <fieldset disabled={busy}>
-          <h2 className="m-auto text-xl bold">Update</h2>
-          <label className="flex items-center gap-2 input input-bordered">
-            Title
+      <fieldset disabled={busy}>
+          <h2 className="m-auto text-xl bold">Create</h2>
+          <label className="flex items-center mt-2 input input-bordered">
+            user Id
             <input
               type="text"
-              name="title"
+              name="userId"
               className="grow"
-              defaultValue={oder.title}
-            />
-          </label>
-          <label className="flex items-center gap-2 input input-bordered">
-            Author
-            <input
-              type="text"
-              name="author"
-              className="grow"
-              defaultValue={oder.author}
-            />
-          </label>
-          <label className="flex items-center gap-2 input input-bordered">
-            Cover
-            <input
-              type="url"
-              name="cover"
-              className="grow"
-              defaultValue={oder.cover}
-            />
-          </label>
-          <label className="field">
-            Content
-            <textarea
-              type="text"
-              name="content"
-              cols="40"
-              rows="10"
-              defaultValue={oder.content}
+              placeholder="Please write userId here"
+              defaultValue={order.userId}
               required
             />
           </label>
+          <label className="flex items-center mt-2 input input-bordered">
+            product Id
+            <input
+              type="text"
+              name="productId"
+              className="grow"
+              placeholder="Please write productId here"
+              defaultValue={order.productId}
+              required
+            />
+          </label>
+          <label className="flex items-center mt-2 input input-bordered">
+          description
+            <input
+              type="url"
+              name="description"
+              className="grow"
+              placeholder="Please write description here"
+              defaultValue={order.description}
+              required
+            />
+          </label>
+          <label className="flex items-center mt-2 input input-bordered">
+            quantity
+            <input
+              type="url"
+              name="quantity"
+              className="grow"
+              placeholder="Please write quantity here"
+              defaultValue={order.description}
+              required
+            />
+          </label>
+          <select className="w-full mt-2 select select-bordered " defaultValue= {order.status}>
+            <option disabled selected>
+              payed
+            </option>
+            <option>Feet_impression</option>
+            <option>3D_Druck</option>
+            <option>shoe_shipped</option>
+            <option>shoe_delivered</option>
+          </select>
         </fieldset>
         <div className="justify-end card-actions">
           <button className="btn" onClick={() => navigate(-1)}>
