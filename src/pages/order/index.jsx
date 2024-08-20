36278e6ca5e6191db3fb7bf50698ce6403906ae3
yongqi8899 <1243994415@qq.com>
2024-08-20 13:26:39 +0200

    change layout

--- src/pages/order/index.jsx
+++ src/pages/order/index.jsx
@@ -5,42 +5,44 @@ export default function Oders() {
   const orders = useLoaderData();
   const navigate = useNavigate();
   return (
-    <div className="grid grid-cols-1 gap-6 md:grid-cols-3 lg:grid-cols-4 justify-items-center">
+    <>
       <button className="btn" onClick={() => navigate(`/orders/create`)}>
         +
       </button>
-      {orders &&
-        orders.map((order) => (
-          <div className="card glass w-1fr" key={order._id}>
-            <div className="card-body">
-              <p>{order.quantity}</p>
-              <div className="flex justify-between mb-2">
-                <p>{order.status}</p>
-                <p>{order.orderDate.split("T")[0]}</p>
-              </div>
-              <div className="justify-end card-actions">
-                <button
-                  className="btn"
-                  onClick={() => navigate(`/orders/${order._id}/update`)}
-                >
-                  update
-                </button>
-                <button
-                  className="btn"
-                  onClick={() => navigate(`/orders/${order._id}/delete`)}
-                >
-                  delete
-                </button>
-                <button
-                  className="btn"
-                  onClick={() => navigate(`/orders/${order._id}`)}
-                >
-                  detail
-                </button>
+      <div className="grid grid-cols-1 gap-6 md:grid-cols-3 lg:grid-cols-4 justify-items-center">
+        {orders &&
+          orders.map((order) => (
+            <div className="card glass w-1fr" key={order._id}>
+              <div className="card-body">
+                <p>{order.quantity}</p>
+                <div className="flex justify-between mb-2">
+                  <p>{order.status}</p>
+                  <p>{order.orderDate.split("T")[0]}</p>
+                </div>
+                <div className="justify-end card-actions">
+                  <button
+                    className="btn"
+                    onClick={() => navigate(`/orders/${order._id}/update`)}
+                  >
+                    update
+                  </button>
+                  <button
+                    className="btn"
+                    onClick={() => navigate(`/orders/${order._id}/delete`)}
+                  >
+                    delete
+                  </button>
+                  <button
+                    className="btn"
+                    onClick={() => navigate(`/orders/${order._id}`)}
+                  >
+                    detail
+                  </button>
+                </div>
               </div>
             </div>
-          </div>
-        ))}
-    </div>
+          ))}
+      </div>
+    </>
   );
 }
