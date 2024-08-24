bfbd581b1914e56f5c6d914e1877f704ad7e5c0d
yongqi8899 <1243994415@qq.com>
2024-08-24 09:56:07 +0200

    fix getallproducts

--- src/data/products/loaders.js
+++ src/data/products/loaders.js
@@ -4,7 +4,7 @@ export const getAllProducts = async () => {
   const data = await res.json();
   return data;
 };
-let allProducts = getAllProducts()
+
 export const getOneProduct = async () => {
   const res = await fetch(`${import.meta.env.VITE_BASE_URL}/products/${id}`);
   if (res.status !== 200) throw Error("something went wrong");
