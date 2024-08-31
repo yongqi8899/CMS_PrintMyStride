deed7e6749f4fdb59589214ec10acf1c41e7ba87
yongqi8899 <1243994415@qq.com>
2024-08-31 13:49:28 +0200

    delete unused code

--- src/pages/order/index.jsx
+++ src/pages/order/index.jsx
@@ -7,76 +7,68 @@ export default function Oders() {
   const path = "orders";
   const navigate = useNavigate();
   return (
-    <>
-      <div>
-        <div className="overflow-x-auto">
-          <table className="table">
-            <thead>
-              <tr>
-                <th>Order ID</th>
-                <th>Username</th>
-                <th>Order Date</th>
-                <th>status</th>
-                <th>Actions</th>
-              </tr>
-            </thead>
-            <tbody>
-              {orders.map((order) => (
-                <tr key={order._id}>
-                  <td>{order._id}</td>
-                  <td>
-                    <div className="flex items-center gap-3">
-                      {/* <div className="avatar">
-                        <div className="mask mask-squircle h-12 w-12">
+    <div className="overflow-x-auto">
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Order ID</th>
+            <th>Username</th>
+            <th>Order Date</th>
+            <th>status</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {orders.map((order) => (
+            <tr key={order._id}>
+              <td>{order._id}</td>
+              <td>
+                <div className="flex items-center gap-3">
+                  {/* <div className="avatar">
+                        <div className="w-12 h-12 mask mask-squircle">
                           <img
                             src="https://img.daisyui.com/images/profile/demo/2@94.webp"
                             alt="Avatar Tailwind CSS Component"
                           />
                         </div>
                       </div> */}
-                      <div>
-                        <div className="font-bold">
-                          {order["userId.userName"]}
-                        </div>
-                      </div>
-                    </div>
-                  </td>
-                  <td>
-                    <span className="badge badge-ghost badge-sm">
-                      {formatDate(order.orderDate)}
-                    </span>
-                  </td>
-                  <td className="align-middle">
-                    <span className="badge badge-secondary">
-                      {order.status}
-                    </span>
-                  </td>
-                  <th>
-                    <button
-                      className="btn btn-ghost btn-xs"
-                      onClick={() => navigate(`/${path}/${order._id}/update`)}
-                    >
-                      update
-                    </button>
-                    <button
-                      className="btn btn-ghost btn-xs"
-                      onClick={() => navigate(`/${path}/${order._id}/delete`)}
-                    >
-                      delete
-                    </button>
-                    <button
-                      className="btn btn-ghost btn-xs"
-                      onClick={() => navigate(`/${path}/${order._id}`)}
-                    >
-                      details
-                    </button>
-                  </th>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-      </div>
-    </>
+                  <div>
+                    <div className="font-bold">{order["userId.userName"]}</div>
+                  </div>
+                </div>
+              </td>
+              <td>
+                <span className="badge badge-ghost badge-sm">
+                  {formatDate(order.orderDate)}
+                </span>
+              </td>
+              <td className="align-middle">
+                <span className="badge badge-secondary">{order.status}</span>
+              </td>
+              <th>
+                <button
+                  className="btn btn-ghost btn-xs"
+                  onClick={() => navigate(`/${path}/${order._id}/update`)}
+                >
+                  update
+                </button>
+                <button
+                  className="btn btn-ghost btn-xs"
+                  onClick={() => navigate(`/${path}/${order._id}/delete`)}
+                >
+                  delete
+                </button>
+                <button
+                  className="btn btn-ghost btn-xs"
+                  onClick={() => navigate(`/${path}/${order._id}`)}
+                >
+                  details
+                </button>
+              </th>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 }
