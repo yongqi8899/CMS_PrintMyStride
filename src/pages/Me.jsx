a36f01a18506979eed7cd62f00e6111b9edc7b07
yongqi8899 <1243994415@qq.com>
2024-09-02 08:54:31 +0200

    fix me

--- src/pages/Me.jsx
+++ src/pages/Me.jsx
@@ -1,25 +1,25 @@
-import { useNavigate } from "react-router-dom";
-import { useAuth } from "@/context";
+import { useAuth } from "@/context/index.js";
 
 export default function Me() {
-    const navigate = useNavigate();
-    const {user} = useAuth();
+  const { user } = useAuth();
   return (
-    <div className="max-w-md m-auto shadow-xl card card-side bg-base-100">
-      <figure>
-        <img
-          src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
-          alt="Movie"
-        />
-      </figure>
-      <div className="card-body">
-        <h2 className="card-title">{user.userName}</h2>
-        <p>Email: {user.email}</p>
-        <p>Role: {user.role}</p>
-        <div className="justify-end card-actions">
-          <button className="btn btn-primary" onClick={() => navigate(`/users/${user._id}/update`)}>Edit</button>
+    user && (
+      <div className="card card-side bg-neutral shadow-xl max-w-xl m-auto">
+        <figure>
+          <img
+            src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+            alt="Movie"
+          />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">{user.userName}</h2>
+          <p>{user.email}</p>
+          <p>{user.role}</p>
+          {/* <div className="card-actions justify-end">
+          <button className="btn btn-primary">Edit</button>
+        </div> */}
         </div>
       </div>
-    </div>
+    )
   );
 }
