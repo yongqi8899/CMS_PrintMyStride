c23e2367becfdf4851d0b775bb7e6e934c0c426e
yongqi8899 <1243994415@qq.com>
2024-09-04 15:23:21 +0200

    add sort

--- src/pages/order/index.jsx
+++ src/pages/order/index.jsx
@@ -19,44 +19,46 @@ export default function Oders() {
           </tr>
         </thead>
         <tbody>
-          {orders.map((order) => (
-            <tr key={order._id}>
-              <td>{order._id}</td>
-              <td>
-                <div className="flex items-center gap-3">
-                  <div className="font-bold">{order["userId.userName"]}</div>
-                </div>
-              </td>
-              <td>
-                <span className="badge badge-ghost badge-sm">
-                  {formatDate(order.orderDate)}
-                </span>
-              </td>
-              <td className="align-middle">
-                <span className="badge badge-secondary">{order.status}</span>
-              </td>
-              <td>
-                <button
-                  className="btn btn-ghost btn-xs"
-                  onClick={() => navigate(`/${path}/${order._id}/update`)}
-                >
-                  update
-                </button>
-                <button
-                  className="btn btn-ghost btn-xs"
-                  onClick={() => navigate(`/${path}/${order._id}/delete`)}
-                >
-                  delete
-                </button>
-                <button
-                  className="btn btn-ghost btn-xs"
-                  onClick={() => navigate(`/${path}/${order._id}`)}
-                >
-                  details
-                </button>
-              </td>
-            </tr>
-          ))}
+          {orders
+            .sort((a, b) => new Date(b.orderDate) - new Date(a.orderDate))
+            .map((order) => (
+              <tr key={order._id}>
+                <td>{order._id}</td>
+                <td>
+                  <div className="flex items-center gap-3">
+                    <div className="font-bold">{order["userId.userName"]}</div>
+                  </div>
+                </td>
+                <td>
+                  <span className="badge badge-ghost badge-sm">
+                    {formatDate(order.orderDate)}
+                  </span>
+                </td>
+                <td className="align-middle">
+                  <span className="badge badge-secondary">{order.status}</span>
+                </td>
+                <td>
+                  <button
+                    className="btn btn-ghost btn-xs"
+                    onClick={() => navigate(`/${path}/${order._id}/update`)}
+                  >
+                    update
+                  </button>
+                  <button
+                    className="btn btn-ghost btn-xs"
+                    onClick={() => navigate(`/${path}/${order._id}/delete`)}
+                  >
+                    delete
+                  </button>
+                  <button
+                    className="btn btn-ghost btn-xs"
+                    onClick={() => navigate(`/${path}/${order._id}`)}
+                  >
+                    details
+                  </button>
+                </td>
+              </tr>
+            ))}
         </tbody>
       </table>
     </div>
